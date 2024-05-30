b5c2460fbe87d1f66721f5707ded65c9a7028e54
0xodus <samuelmsuu@gmail.com>
2024-05-30 21:51:48 +0300

    feat: Update BEP20Basic.sol contract with OpenZeppelin integration

--- BEP20/BEP20Basic.sol
+++ BEP20/BEP20Basic.sol
@@ -1,3 +1,37 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract BEP20Basic is IERC20 {
+    string public name;
+    string public symbol;
+    uint8 public constant decimals = 18;
+
+    mapping (address => uint256) balances;
+
+    mapping (address => mapping (address => uint256)) allowances;
+
+    uint256 _totalSupply = 100000000 * 10**18; //100mln
+
+    constructor(string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
+
+    }
+    function totalSupply() public view override returns (uint256) {
+        return totalSupply;
+    }
+
+    function balanceOf(address tokenOwner)public view override returns(uint256){
+        return balances[tokenOwner];
+    }
+    function transfer(address reciever, uint256 numTokens) public override returns (bool){
+        require(numtokens <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender] - numTokens;
+        balances[reciever] = balances[reciever] + numTokens;
+        emit Transfer(msg.sender,reciever, numtokens);
+        return true;
+    }
+}

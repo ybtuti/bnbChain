188703c04f87fa51ba9c718da08722fca8780d6e
0xodus <samuelmsuu@gmail.com>
2024-06-02 12:05:41 +0300

    feat: Add test for verifying whitelist address in Airdrop contract

--- Airdrop/test/AirdropTest.js
+++ Airdrop/test/AirdropTest.js
@@ -9,3 +9,40 @@ function enclodeLeaf(address, spots) {
     [address, spots] //actual values
   );
 }
+describe("Merkle Trees", function () {
+  it("Should be able to verify if the address is in the whitelist or not", async function () {
+    const testAddresses = await ethers.getSigners();
+
+    const List = [
+      enclodeLeaf(testAddresses[0].address, 2),
+      enclodeLeaf(testAddresses[1].address, 2),
+      enclodeLeaf(testAddresses[2].address, 2),
+      enclodeLeaf(testAddresses[3].address, 2),
+      encodeLeaf(testAddresses[5].address, 2),
+    ];
+    const merkleTree = new MerkleTree(List, keccak256, {
+      hashLeaves: true,
+      sortPairs: true,
+      sortLeaves: true,
+    });
+
+    const root = merkleTree.getHexRoot();
+
+    const airdrop = await ethers.getContractFactory("Airdrop");
+
+    const Airdrop = await airdrop.deploy(root);
+    await Airdrop.waitForDeploymet();
+
+    for (let i = 0; i < 6; i++) {
+      const leaf = keccak256(List[i]);
+      const proof = merkleTree.getHexProof(leaf);
+
+      const connectedAirdrop = await Airdrop.connect(testAddresses[i]);
+
+      expect(verified).to.equal(true);
+    }
+
+    const verifiedInvalid = await Airdrop.checkwhitelist([], 2);
+    expect(verifiedInvalid).to.equal(false);
+  });
+});

305ed488a62c4e9a2bf1b4f43c71cef313f41450
0xodus <samuelmsuu@gmail.com>
2024-06-02 11:14:40 +0300

    feat: Add airdropWithTransfer function to AirdropToken contract

--- Airdrop/AirdropToken.sol
+++ Airdrop/AirdropToken.sol
@@ -2,3 +2,17 @@
 
 pragma solidity ^0.8.0;
 
+interface IfakeToken {
+    function transfer (address _to, uint256 _amount) external;
+
+    function transferFrom(address _from, address _to, uint256 _amount) external;
+
+}
+
+contract AirdropToken {
+    function airdropWithTransfer(IfakeToken _token, address[] memory _addressArray, uint[] memory _amountArray) public{
+        for(uint i = 0; i < _addressArray.length; i++){
+            _token.transfer(_addressArray[i], _amountArray[i]);
+        }
+    }
+}

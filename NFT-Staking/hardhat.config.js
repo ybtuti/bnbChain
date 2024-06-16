ce475ff38ee48560e49f7c6bba93d607f490b6b9
0xodus <samuelmsuu@gmail.com>
2024-06-16 14:32:14 +0300

    feat: Update solidity version to 0.8.21 and add BSC testnet and Etherscan configurations

--- NFT-Staking/hardhat.config.js
+++ NFT-Staking/hardhat.config.js
@@ -1,6 +1,38 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const SECRET_KEY = process.env.SECRET_KEY;
+
+/**@type import ('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  defaultNetwork: "sepolia",
+  networks: {
+    bnbTestnet: {
+      url: "https://bsc-testnet-rpc.publicnode.com",
+      chainId: 97,
+      accounts: [SECRET_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  solidity: {
+    version: "0.8.21",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 40000,
+  },
 };

f16139228f312bba1c7173e1930c29b5b9f3b3fd
0xodus <samuelmsuu@gmail.com>
2024-06-16 14:49:55 +0300

    feat: Add staking functionality to NFTStaking contract
    This commit adds the staking functionality to the NFTStaking contract. Users can now stake their NFT tokens by approving the contract to spend their tokens and calling the stake function. This feature enables users to participate in the staking mechanism and earn rewards.

--- NFT-Staking/test/NFTStaking.js
+++ NFT-Staking/test/NFTStaking.js
@@ -8,4 +8,35 @@ describe("NFTStaking", function () {
   let user2;
   let NFTCollection;
   let RewardsToken;
+
+  beforeEach(async () => {
+    [owner, user1, user2] = await ethers.getSigners();
+
+    const rewardsToken = await ethers.getContractFactory("RewardsToken");
+    RewardsToken = await rewardsToken.deploy();
+
+    const nftCollection = await ethers.getContractFactory("NFTCollection");
+    NFTCollection = await nftCollection.deploy();
+
+    NFTStaking = await ethers.getContractFactory("NFTStaking");
+    nftStaking = await ethers.deployContract(
+      "NFTStaking",
+      [NFTCollection.target, RewardsToken.target],
+      {}
+    );
+
+    await nftStaking.waitForDeployment();
+    await NFTCollection.mint(user1);
+  });
+  it("Should allow users to stake tokens", async () => {
+    const tokenIdsToStake = [1];
+    await NFTCollection.connect(user1).approve(
+      nftStaking.target,
+      tokenIdsToStake[0]
+    );
+    await nftStaking.connect(user1).stake(tokenIdsToStake);
+    const staker = await nftStaking.stakers(user1.address);
+    const result = await nftStaking.isStaked(user1, tokenIdsToStake[0]);
+    expect(result).to.equal(true);
+  });
 });
